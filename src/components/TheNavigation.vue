b79de2d2177e3b86beaaadb360300eb4cfaa76f2
JingPoo <jingh861219@gmail.com>
2023-07-25 18:02:08 +0800

    update navbar toggle button

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -29,8 +29,9 @@ const navclickHandler = (text)=>{
     <div class="nav-container min-w-fit bg-primary text-white sticky top-0 z-40 md:fixed transition ease-in duration-150" :class="{'md:-translate-x-full': !navShow}">
         <nav class="h-16 w-full md:h-screen md:w-44 text-md flex flex-row md:flex-col justify-start items-center shadow relative">
             <!-- toggle button -->
-            <button class="w-10 h-24 bg-complementary text-black border-y-2 border-r-2 border-primary rounded-r-xl flex justify-center items-center absolute left-full top-8 opacity-80 hover:opacity-70 invisible md:visible" @click="clickHandler">
-                <svg class="w-10 h-10" :class="{'-rotate-180': !navShow}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" ><path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" /></svg>
+            <button class="z-50 w-full h-16 outline-none bg-complementary text-black  flex justify-center items-center absolute left-0 bottom-0  hover:opacity-80 invisible md:visible" :class="{'!w-10 h-full left-full top-0 border-y-2 border-r-2 border-primary rounded-r-2xl': !navShow}" @click="clickHandler">
+                <!-- <svg class="w-10 h-10" :class="{'-rotate-180': !navShow}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" ><path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" /></svg> -->
+                <svg class="w-10 h-10" :class="{'-rotate-180': !navShow}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" ><path stroke-linecap="round" stroke-linejoin="round" d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5" /></svg>
             </button>
 
             <!-- home -->
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -64,6 +64,10 @@ const favorHandler = ((id)=>{
     refreshkey.value += 1
   }
 })
+const roleclickHandler = (text) => {
+  selectRole.value = text
+  localStorage.setItem('role', JSON.stringify(text))
+}
 </script>
 
 <template>
@@ -82,7 +86,7 @@ const favorHandler = ((id)=>{
       <div class="role-box w-full min-w-max h-12 text-gray-500 font-semibold bg-gray-200 border-x-2 md:border-t-2 border-gray-500 md:rounded-tr-lg flex justify-center items-center">
         <button v-for="role in roles" 
         :key="role.id" 
-        @click="selectRole = role.text"
+        @click="roleclickHandler(role.text)"
         class="w-[12.5%] min-w-fit h-full text-xl hover:text-gray-800 hover:font-extrabold border-r-2 border-gray-500 first:border-l-0 last:border-r-0 last:md:rounded-tr-lg flex justify-center items-center group"
         :class="{'!text-2xl text-gray-800 bg-white font-extrabold transition-all duration-150 ease-out': selectRole === role.text}">
           <svg class="w-6 h-6 hidden group-hover:text-yellow-600" :class="{'lg:!block': role.text === '收藏'}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" ><path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" /></svg>
