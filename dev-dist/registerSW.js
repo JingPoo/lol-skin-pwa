0213fb0fe5a07b06e8645c483b76ce4c3d9c503c
JingPoo <jingh861219@gmail.com>
2023-09-07 14:56:33 +0800

    add pwa setting

---
+++ dev-dist/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) navigator.serviceWorker.register('/vue-lol-skins/dev-sw.js?dev-sw', { scope: '/vue-lol-skins/', type: 'classic' })
--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,61 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/vue-lol-skins/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      manifest: {
+        name: "PWA-lol-skin",
+        description: "lol skin pwa",
+        theme_color: "#00bd7e",
+        icons: [
+          {
+            src: "/ls_192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/ls_512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/ls_60.png",
+            sizes: "60x60",
+            type: "image/png",
+          },
+        ],
+      },
+      // shortcuts: [
+      //     {
+      //       name: "Open About",
+      //       short_name: "About",
+      //       description: "Open the about page",
+      //       url: "/about",
+      //       icons: [{ src: "/App_icon192.png", sizes: "192x192" }],
+      //     },
+      //     {
+      //       name: "Report issue",
+      //       short_name: "Report",
+      //       description: "Open the issue report page",
+      //       url: "/report",
+      //       icons: [{ src: "/App_icon192.png", sizes: "192x192" }],
+      //     },
+      // ],
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
